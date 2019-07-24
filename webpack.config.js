586b2d3613525eb21c79f0ecf3b2065991eeb53c
Isac <isac.junior@ifood.com.br>
2019-07-24 13:33:13 -0300

    upgrade some dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { name } = require('./package.json')
 
 module.exports = {
 
@@ -10,7 +12,7 @@ module.exports = {
   mode: 'development',
 
   entry: {
-    main: './src/index.tsx',
+    [`${name}-main`]: './src/index.tsx',
   },
 
   output: {
@@ -41,14 +43,8 @@ module.exports = {
 
   optimization: {
     splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /node_modules/,
-          name: 'vendor',
-          chunks: 'initial',
-          enforce: true,
-        },
-      },
+      chunks: 'all',
+      name: `${name}-vendor`,
     },
   },
 
@@ -57,6 +53,7 @@ module.exports = {
       title: 'React Typescript Babel 7',
       template: './src/index.html',
     }),
+    new CleanWebpackPlugin(),
   ],
 
 }
